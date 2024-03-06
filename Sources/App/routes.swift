f14fef2fe4c1ef2451f142f5cc1fb91b579fbaeb
Ibrahim Hosseini <sehosseini@me.com>
2024-03-06 18:59:37 +0330

    first commit on Vapor sample app

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,8 +6,9 @@ func routes(_ app: Application) throws {
         try await req.view.render("index", ["title": "Hello Vapor!"])
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    app.get("hello", ":name") { req async throws -> String in
+        let name = try req.parameters.require("name")
+        return "Hello, \(name.capitalized)!"
     }
 
     try app.register(collection: TodoController())
