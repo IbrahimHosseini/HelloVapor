ad82b4284d6692a5b203cc5c1fd1e86995e91e31
Ibrahim Hosseini <sehosseini@me.com>
2024-03-07 09:26:25 +0330

    create a POST API

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,9 +6,19 @@ func routes(_ app: Application) throws {
         try await req.view.render("index", ["title": "Hello Vapor!"])
     }
 
+    /// GET API
     app.get("json", ":name") { req async throws -> UserResponse in
         let name = try req.parameters.require("name")
         let message = "Hello, \(name.capitalized)!"
+        return UserResponse(message: message)
+    }
+    
+    /// POST API
+    app.post("user-info") { req async throws -> UserResponse in
+        let userInfo = try req.content.decode(UserInfo.self)
+        
+        let message = "Hello, \(userInfo.name.capitalized)! You are \(userInfo.age) years old."
+        
         return UserResponse(message: message)
     }
 
@@ -17,4 +27,9 @@ func routes(_ app: Application) throws {
 
 struct UserResponse: Content {
     let message: String
+}
+
+struct UserInfo: Content {
+    let name: String
+    let age: Int
 }
